8932a1a154ede7ecf6d0ca840a70aee344860b86
João Pedro <joaof082@gmail.com>
2025-04-23 01:39:15 -0300

    feat: adding style global and removing codes that not used
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Theme from './views/styles/theme'
+import Routes from './routes'
 
-import Routes from "./routes";
-import Theme from "./views/styles/theme";
-import GlobalStyle from "./views/styles/globalStyles";
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
 
-const App = () => (
-  <ThemeProvider theme={Theme}>
-    <GlobalStyle />
-    <Routes />
-  </ThemeProvider>
-);
-export default App;
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+`
+
+function App() {
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
+  )
+}
+
+export default App
