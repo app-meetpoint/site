83b3903a4f95c7fbe6ba6382b61709de1457c54b
Calenzo <calenzodevs@gmail.com>
2025-07-11 10:29:10 -0300

    deploy prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   define: { _global: {} },
+  build: {
+    outDir: "build",
+  },
 });
